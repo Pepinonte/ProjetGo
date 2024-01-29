d6788a68eac33124178476a21b472d89c582ee71
Gonni <78425638+G0nni@users.noreply.github.com>
2024-01-29 11:36:12 +0100

    addLogs in fileManagement + gitignore

--- src/app/filesmanagement/deleteFile.go
+++ src/app/filesmanagement/deleteFile.go
@@ -1,19 +1,31 @@
 package filesmanagement
 
 import (
+	"app/database"
 	"fmt"
 	"os"
+	"time"
 )
 
 func DeleteFile(path string) (string, error) {
+
+	database.Condb()
+	currentTime := time.Now()
+	output := "success"
+
 	if path == "" {
+		output = "path vide"
 		return "", fmt.Errorf("le nom du fichier ne peut pas être vide")
 	}
 	err := os.Remove(path)
 	if err != nil {
+		output = "impossible de supprimer le fichier"
 
 		fmt.Println("impossible de supprimer le fichier")
 		return "", err
 	}
+
+	database.AddLog(currentTime.Format("2006.01.02 15:04:05"), "filesmanagement", "DeleteFile()", path, output)
+
 	return "succes",nil
 }
