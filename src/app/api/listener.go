b1a9e2212f9f60b795b356bf585a6205a9834ccf
GROS Christian <christian-gros@outlook.fr>
2024-01-29 09:36:46 +0100

    ajout de toutes les routes folder

--- src/app/api/listener.go
+++ src/app/api/listener.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"app/database"
 	"app/foldermanagement"
 
 	"github.com/gin-gonic/gin"
@@ -8,13 +9,35 @@ import (
 
 func RunListener() {
 	router := gin.Default()
-	router.GET("/createFolder/:name", createFolder)
+	router.GET("showLogs", showLogs)
+	router.POST("/createFolder/:name", createFolder)
+	router.DELETE("/deleteFolder/:name", deleteFolder)
+	router.GET("/readFolder/:name", readFolder)
+	router.PUT("/renameFolder/:lname/:nname", renameFolder)
 	router.Run("localhost:8080")
 }
-
 
 func createFolder(c *gin.Context) {
 	name := c.Param("name")
 	foldermanagement.CreateFolder(name)
-		
+}
+
+func deleteFolder(c *gin.Context) {
+	name := c.Param("name")
+	foldermanagement.DeleteFolder(name)
+}
+
+func readFolder(c *gin.Context) {
+	name := c.Param("name")
+	foldermanagement.ReadFolder(name)
+}
+
+func renameFolder(c *gin.Context) {
+	lname := c.Param("lname")
+	nname := c.Param("nname")
+	foldermanagement.RenameFolder(lname, nname)
+}
+
+func showLogs(c *gin.Context) {
+	database.GetLogs()
 }
--- src/app/foldermanagement/createFolder.go
+++ src/app/foldermanagement/createFolder.go
@@ -1,19 +1,28 @@
 package foldermanagement
 
 import (
+	"app/database"
 	"errors"
 	"os"
+	"time"
 )
 
 var ErrPathVoid = errors.New("path vide")
 
 func CreateFolder(path string)  (string, error){
 	err := os.Mkdir(path, 0755)
+	database.Condb()
+	currentTime := time.Now()
+	
 	if path == ""  {
 		return "",errors.New("path vide")
 	}
 	if err != nil {
-		return "",err
+		database.AddLog(currentTime.Format("2006.01.02 15:04:05"), "foldermanagement", "CreateFolder()", path, "success")
+
+		return "", err
 	}
-	return "Dossier créé", nil
+	return "success", nil
 }
+
+
